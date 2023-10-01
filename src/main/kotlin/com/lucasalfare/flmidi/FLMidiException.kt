7387a9d67503cde280e302d9b417bfd320a61655
LucasAlfare <god-lucas@hotmail.com>
2023-09-30 22:39:40 -0300

    removed named argument from `Exception` inheritance

--- src/main/kotlin/com/lucasalfare/flmidi/FLMidiException.kt
+++ src/main/kotlin/com/lucasalfare/flmidi/FLMidiException.kt
@@ -3,4 +3,4 @@ package com.lucasalfare.flmidi
 /**
  * Defines the base exception that could occur over the library flow.
  */
-class FLMidiException(message: String): Exception(message = message)
+class FLMidiException(message: String): Exception(message)
