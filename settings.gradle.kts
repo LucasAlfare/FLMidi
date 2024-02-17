f3728f22802b5885710146ec7e9c79b3bba79072
Francisco Lucas <god-lucas@hotmail.com>
2024-02-17 13:21:58 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "FLMidi"
+
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+  }
+}
