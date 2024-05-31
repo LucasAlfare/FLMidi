233527c9284b9839b97fd3c675d1bb6858e8916c
LucasAlfare <god-lucas@hotmail.com>
2024-05-31 00:41:47 -0300

    cleaning names

--- src/main/kotlin/com/lucasalfare/flmidi/Decoder.kt
+++ src/main/kotlin/com/lucasalfare/flmidi/Decoder.kt
@@ -4,39 +4,42 @@ import com.lucasalfare.flbinary.Reader
 import java.io.File
 import kotlin.math.pow
 
-open class MyEvent
+open class Event
 
-data class TrackMetaEvent(
+data class MetaEvent(
   val type: Int,
   val delta: Int,
   val data: Any? = null
-) : MyEvent() {
-  override fun toString() = "TrackMetaEvent(type=${type.toHexString()}, delta=$delta, data=$data)"
+) : Event() {
+  override fun toString() = "MetaEvent(type=${type.toHexString()}, delta=$delta, data=$data)"
 }
 
-data class TrackControlEvent(
+data class ControlEvent(
   val type: Int,
   val delta: Int,
   val data: Any? = null,
   val targetChannel: Int = type and 0b1111
-) : MyEvent() {
+) : Event() {
   override fun toString() =
-    "TrackControlEvent(type=${type.toBinaryString()}, delta=$delta, data=$data, targetChannel=$targetChannel)"
+    "ControlEvent(type=${type.toBinaryString()}, delta=$delta, data=$data, targetChannel=$targetChannel)"
 }
 
-data class MyTrack(
+data class Track(
   val signature: String,
   val length: Long,
-  val events: MutableList<MyEvent>
+  val events: MutableList<Event>
 ) {
-  override fun toString() = buildString {
-    append("\tSignature: $signature\n")
-    append("\tLength: $length\n")
-    events.forEach { append("\t$it"); append("\n") }
+  override fun toString(): String {
+    val t = this
+    return buildString {
+      append("\tSignature: $signature\n")
+      append("\tLength: ${t.length}\n")
+      events.forEach { append("\t$it"); append("\n") }
+    }
   }
 }
 
-data class MyHeader(
+data class Header(
   val signature: String,
   val length: Long,
   val midiFormat: Int,
@@ -46,11 +49,14 @@ data class MyHeader(
 
 @OptIn(ExperimentalUnsignedTypes::class)
 fun main() {
-  val file = File("midi_format_1_example.mid")
+//  specFileFormat1()
+
+//  val file = File("midi_format_1_example.mid")
+  val file = File("example.mid")
   if (file.exists() && !file.isDirectory) {
     val reader = Reader(file.readBytes().toUByteArray())
 
-    val myHeader = MyHeader(
+    val header = Header(
       signature = reader.readString(4) ?: "no signature",
       length = reader.read4Bytes(),
       midiFormat = reader.read2Bytes(),
@@ -58,17 +64,22 @@ fun main() {
       timeDivision = reader.read2Bytes()
     )
 
-    println(myHeader)
+    println(header)
 
-    println("~Warning: this is a midi file in format ${myHeader.midiFormat}!~")
+    println("~Warning: this is a midi file in format ${header.midiFormat}!~")
 
-    val tracks = mutableListOf<MyTrack>()
+    if (header.midiFormat != 0) {
+      error("Only MIDI files of type 0 are accepted!")
+    }
 
-    for (i in 0..<myHeader.numTracks) {
+    val tracks = mutableListOf<Track>()
+
+    for (i in 0..<header.numTracks) {
       val trackSignature = reader.readString(4)
       val trackLength = reader.read4Bytes()
+
       var previousStatus = 0
-      val trackEvents = mutableListOf<MyEvent>()
+      val trackEvents = mutableListOf<Event>()
       while (true) {
         val delta = reader.readVariableLengthValue()
         var currentStatus = reader.read1Byte()
@@ -85,7 +96,7 @@ fun main() {
               0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 -> {
                 val textLength = reader.readVariableLengthValue()
                 val data = reader.readString(textLength)!!
-                trackEvents += TrackMetaEvent(type, delta, data)
+                trackEvents += MetaEvent(type, delta, data)
               }
 
               // time signature
@@ -104,14 +115,14 @@ fun main() {
                   numberOf32ndNotesIn24MidiClocks
                 )
 
-                trackEvents += TrackMetaEvent(type, delta, data)
+                trackEvents += MetaEvent(type, delta, data)
               }
 
               // set tempo in microseconds per quarter note
               0x51 -> {
                 val numDataItems = reader.read1Byte()
                 val tempoInMicroseconds = reader.read3Bytes()
-                trackEvents += TrackMetaEvent(type, delta, tempoInMicroseconds)
+                trackEvents += MetaEvent(type, delta, tempoInMicroseconds)
               }
 
               // SMPTE Offset
@@ -125,14 +136,14 @@ fun main() {
                   reader.read1Byte()
                 )
 
-                trackEvents += TrackMetaEvent(type, delta, data)
+                trackEvents += MetaEvent(type, delta, data)
               }
 
               // key signature
               0x59 -> {
                 val dataLength = reader.read1Byte()
                 val data = listOf(reader.read1Byte(), reader.read1Byte())
-                trackEvents += TrackMetaEvent(type, delta, data)
+                trackEvents += MetaEvent(type, delta, data)
               }
 
 
@@ -140,7 +151,7 @@ fun main() {
               0x20 -> {
                 val dataLength = reader.read1Byte()
                 val currentEffectiveMidiChannel = reader.read1Byte()
-                trackEvents += TrackMetaEvent(type, delta, currentEffectiveMidiChannel)
+                trackEvents += MetaEvent(type, delta, currentEffectiveMidiChannel)
               }
 
               // sequencer specific meta event
@@ -151,12 +162,12 @@ fun main() {
                   auxBytes += reader.read1Byte()
                 }
 
-                trackEvents += TrackMetaEvent(type, delta, auxBytes)
+                trackEvents += MetaEvent(type, delta, auxBytes)
               }
 
               // end of track
               0x2F -> {
-                trackEvents += TrackMetaEvent(type, delta)
+                trackEvents += MetaEvent(type, delta)
                 break
               }
 
@@ -178,7 +189,7 @@ fun main() {
               // select instrument
               0b1100 -> {
                 val targetInstrument = reader.read1Byte()
-                trackEvents += TrackControlEvent(type, delta, targetInstrument)
+                trackEvents += ControlEvent(type, delta, targetInstrument)
               }
 
               // note on, note off
@@ -186,7 +197,7 @@ fun main() {
                 val noteNumber = reader.read1Byte() and 0b01111111
                 val noteVelocity = reader.read1Byte() and 0b01111111
                 val data = listOf(noteNumber, noteVelocity)
-                trackEvents += TrackControlEvent(type, delta, data)
+                trackEvents += ControlEvent(type, delta, data)
               }
 
               // channel mode
@@ -199,19 +210,19 @@ fun main() {
                   channelModeArg1,
                   channelModeArg2
                 )
-                trackEvents += TrackControlEvent(type, delta, data)
+                trackEvents += ControlEvent(type, delta, data)
               }
 
               // channel pressure
               0b1101 -> {
                 val channelPressure = reader.read1Byte()
-                trackEvents += TrackControlEvent(type, delta, channelPressure)
+                trackEvents += ControlEvent(type, delta, channelPressure)
               }
 
               // pitch bend
               0b1110 -> {
                 val pitchBend = reader.read1Byte()
-                trackEvents += TrackControlEvent(type, delta, pitchBend)
+                trackEvents += ControlEvent(type, delta, pitchBend)
               }
 
               else -> {
@@ -221,9 +232,9 @@ fun main() {
           }
         }
       }
-      tracks += MyTrack(trackSignature ?: "no signature", trackLength, trackEvents)
+      tracks += Track(trackSignature ?: "no signature", trackLength, trackEvents)
 
-      break
+//      break
     }
 
     tracks.forEachIndexed { index, myTrack ->
@@ -236,7 +247,7 @@ fun main() {
 @OptIn(ExperimentalUnsignedTypes::class)
 fun specFileFormat1() {
   val s =
-    "4D 54 68 64 00 00 00 06 00 01 00 04 00 60 4D 54 72 6B 00 00 00 14 FF 58 04 04 02 18 08 FF 51 03 07 A1 20 FF 2F 00 4D 54 72 6B 00 00 00 10 C0 05 90 4C 20 4C 00 FF 2F 00 4D 54 72 6B 00 00 00 0F C1 2E 91 43 40 43 00 FF 2F 00 4D 54 72 6B 00 00 00 15 C2 46 92 30 60 3C 60 30 00 3C 00 FF 2F 00"
+    "4D 54 68 64 00 00 00 06 00 01 00 04 00 60 4D 54 72 6B 00 00 00 14 00 FF 58 04 04 02 18 08 00 FF 51 03 07 A1 20 83 00 FF 2F 00 4D 54 72 6B 00 00 00 10 00 C0 05 81 40 90 4C 20 81 40 4C 00 00 FF 2F 00 4D 54 72 6B 00 00 00 0F 00 C1 2E 60 91 43 40 82 20 43 00 00 FF 2F 00 4D 54 72 6B 00 00 00 15 00 C2 46 00 92 30 60 00 3C 60 83 00 30 00 00 3C 00 00 FF 2F 00"
       .replace("\t", "")
       .replace("\n", " ")
       .replace("  ", " ")
