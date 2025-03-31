f9c99af047b018eb26515ebeebaa76e26573106c
LucasAlfare <god-lucas@hotmail.com>
2025-03-31 00:04:38 -0300

    clean up

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,1 @@
 rootProject.name = "FLMidi"
-
-//dependencyResolutionManagement {
-//  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-//  repositories {
-//    mavenCentral()
-//    maven("https://jitpack.io")
-//  }
-//}
