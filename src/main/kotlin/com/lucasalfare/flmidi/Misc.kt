022ca14f0aed4f3fb6bf161406c7e6bbbf65d493
LucasAlfare <god-lucas@hotmail.com>
2025-03-30 23:56:09 -0300

    remove unused file

--- src/main/kotlin/com/lucasalfare/flmidi/Misc.kt
+++
@@ -1,6 +0,0 @@
-package com.lucasalfare.flmidi
-
-fun Int.toHexString() = "0x${Integer.toHexString(this).padStart(2, '0')}"
-
-// hardcoded padding only to 4 units
-fun Int.toBinaryString() = "0b${Integer.toBinaryString(this).padStart(4, '0')}"
