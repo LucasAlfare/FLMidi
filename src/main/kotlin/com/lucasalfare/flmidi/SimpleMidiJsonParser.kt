ad5bac2efc7fd65d26a6c8ec4ef680d3f4e3acef
LucasAlfare <god-lucas@hotmail.com>
2025-04-01 15:09:29 -0300

    improve data structure types; fix toString() errors;

--- src/main/kotlin/com/lucasalfare/flmidi/SimpleMidiJsonParser.kt
+++ src/main/kotlin/com/lucasalfare/flmidi/SimpleMidiJsonParser.kt
@@ -7,12 +7,12 @@ object SimpleMidiJsonParser {
   private var json = Json { prettyPrint = false }
   private var prettyPrintJson = Json { prettyPrint = true }
 
-  fun getJsonStringFromMidi(midi: Midi, formatted: Boolean = false): String {
+  fun midiToJson(midi: Midi, formatted: Boolean = false): String {
     if (formatted) return prettyPrintJson.encodeToString(midi)
     return json.encodeToString(midi)
   }
 
-  fun getMidiFromJsonString(jsonString: String): Midi {
+  fun jsonToMidi(jsonString: String): Midi {
     return json.decodeFromString<Midi>(jsonString)
   }
 }
